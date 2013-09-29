752783ea4d4b9382419c12ecb93a887277b7306b
Matthew Hall <hailmattyhalluk@gmail.com>
2013-09-29 16:39:04 +0100

    Add FLICKR_API variables to flickr.py

--- flickr.py
+++ flickr.py
@@ -34,6 +34,8 @@ HOST = 'http://flickr.com'
 API = '/services/rest'
 
 # set these here or using flickr.API_KEY in your application
+API_KEY = ''
+API_SECRET = ''
 email = None
 password = None
 AUTH = False
